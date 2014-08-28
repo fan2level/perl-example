075f3749bca474bde8fa810f5fb3ac7784916cf7
fan2level <asinwolf@gmail.com>
2014-08-29 01:29:50 +0900

    tkx entry widget
    fix last input valid

--- tkx/tkx-entry.pl
+++ tkx/tkx-entry.pl
@@ -1,20 +1,22 @@
 #!/usr/bin/perl -w
+# ref : http://www.perlmonks.org/?node_id=881505
 
 use strict;
 use Tkx;
 
+my $input;
+my $result;
+
 my $wMain = Tkx::widget->new(".");
 $wMain->g_wm_title("entry widget");
-
-my $input;
-my $wgEntry = $wMain->new_tk__entry(-width=>80,
-				    -textvariable=>\$input,
-				    -validate=>"key", # "none" "key" "focus" "focusin" "focusout" "all"
-				    -validatecommand=>\&command_validate,
-				    -invalidcommand=>\&command_invalid,
+my $wgFrame = $wMain->new_tk__frame();
+my $wgEntry = $wgFrame->new_tk__entry(-width=>80,
+				      -textvariable=>\$input,
+				      -validate=>"key", # "none" "key" "focus" "focusin" "focusout" "all"
+				      -validatecommand=>[\&command_validate, Tkx::Ev('%P')],
+				      -invalidcommand=>\&command_invalid,
 				   );
-my $result;
-my $wgLabel= $wMain->new_tk__label(-textvariable=>\$result);
+my $wgLabel= $wgFrame->new_tk__label(-textvariable=>\$result);
 
 my $wSub = $wMain->new_toplevel;
 $wSub->g_wm_title("debug window");
@@ -23,6 +25,7 @@ my $wgText = $wSub->new_tk__text(-state=>"disabled",
 				 -height=>24,
 				 -wrap=>"none");
 
+$wgFrame->g_grid;
 $wgEntry->g_grid(-column=>0, -row=>0, -sticky=>"we");
 $wgLabel->g_grid(-column=>0, -row=>1, -sticky=>"w");
 $wgText->g_grid();
@@ -38,13 +41,12 @@ $wgEntry->g_focus();
 Tkx::MainLoop();
 
 sub command_validate{
-  $result = $input;
+  $result = shift @_;
   return 1;
 }
 
 sub command_invalid{
   # call when command_validate() returns 0
-
   debugTo("invalid command ...\n");
 }
 
